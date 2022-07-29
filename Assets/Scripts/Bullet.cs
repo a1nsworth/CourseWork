d62b148499f9af0bb50b7ed271e7b8479df9c314
Daniel Wahbi <xed255@gmail.com>
2022-07-29 23:36:18 +0300

    Добавлен класс для контроля пули

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,33 +5,31 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-// {
-//     [SerializeField] private float speed;
-//     [SerializeField] private int damage;
+    [SerializeField] private float speed;
+    [SerializeField] private int damage;
 
-//     private Rigidbody2D rb;
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         rb.velocity = transform.right * speed;
-//     }
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
 
-//     void Update()
-//     {
-//         if (Math.Abs(transform.position.x) > Math.Abs(10) || Math.Abs(transform.position.y) > Math.Abs(10))
-//         {
-//             Destroy(gameObject);
-//         }
-//     }
+    private void Update()
+    {
+        if (Math.Abs(transform.position.x) > 10 || Math.Abs(transform.position.y) > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
 
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         var player = other.GetComponent<Player>();
-//         if (player)
-//         {
-//             player.GetDamage(damage);
-//         }
-
-//         Destroy(gameObject);
-//     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<Player>();
+        if (player)
+        {
+            player.GetDamage(damage);
+        }
+        Destroy(gameObject);
+    }
 }
