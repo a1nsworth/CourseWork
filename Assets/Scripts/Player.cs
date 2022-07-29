8dd79577e7f39c7cf42d136e91c2e343057bf939
Daniel Wahbi <xed255@gmail.com>
2022-07-29 23:35:42 +0300

    Добавлен класс Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speedX;
-    public float jumpForce;
+    [SerializeField] private int basicHealth;
+    [SerializeField] private int basicLifes;
+    public int Health { get; set; }
+    public int Lifes { get; set; }
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        Health = basicHealth;
+        Lifes = basicLifes;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void GetDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            if (Lifes > 1)
+                Lifes--;
+            else
+                Die();
+        }
+
+        Debug.Log(Health);
+    }
+
+    public void Die()
+    {
+        Destroy(rb.gameObject);
+    }
 }
